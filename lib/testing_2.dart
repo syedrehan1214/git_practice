4fb2f1144ed14620ad3299b915a4b55e06cce91a
syedrehan1214 <kazmi.micromerger@gmail.com>
2024-06-03 17:28:00 +0500

    testing two class

---
+++ lib/testing_2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TestingTwo extends StatelessWidget {
+  const TestingTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+}
